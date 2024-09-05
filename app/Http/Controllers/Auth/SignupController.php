1e05676e115b5aeee173501a4af74ac6588e2eed
rawan <rawanesmandar0@gmail.com>
2024-09-05 21:04:00 +0300

    second upload

--- app/Http/Controllers/Auth/SignupController.php
+++ app/Http/Controllers/Auth/SignupController.php
@@ -18,22 +18,7 @@ use App\Models\User;
 class SignupController extends Controller
 {
     use HttpResponses;
-    //use FileUploadTrait;
-
-    public function uploadFile(UploadedFile $file, $directory, $filename = null)
-    {
-        $filename = $filename ?? $this->generateFileName($file);
-        $file->move(public_path($directory), $filename);
-        return $directory . '/' . $filename;
-    }
-
-    protected function generateFileName(UploadedFile $file)
-    {
-        $timestamp = time();
-        $extension = $file->getClientOriginalExtension();
-        $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-        return "{$filename}_{$timestamp}.{$extension}";
-    }
+    use FileUploadTrait;
 
     public function register(StoreUserRequest $request)
     {
